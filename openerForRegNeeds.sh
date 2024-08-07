84470ffec8903b8259944b532026109f68176d6f
alexander <Guakocius.engelhardt.alexander@mein.gmx>
2024-08-07 10:03:06 +0200

    Initialization of repo + first .sh scripts

---
+++ openerForRegNeeds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Open Firefox
+firefox &
+
+# Open Google Chrome
+google-chrome &
+
+# Open Terminal
+gnome-terminal -- bash &
---
+++ startup_alert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Display an alert with the message
+gnome-terminal -- bash -c "echo 'Ask Vitalij about Macbook + Create GH Repo for all scripts'; exec bash"
